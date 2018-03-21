c063b131d39c000fe4c9847ee87c179411c02ef7
Ankan Biswas <ankanb06@gmail.com>
2018-03-21 17:41:13 +0530

    Updating figure 4 backup before

---
+++ Programs/biofeedbackFigure4_back_before_removing_first_subplot.m
@@ -0,0 +1,49 @@
+% biofeedbackFigure4(subjectName,folderName)
+% Main aim of the code is to plot
+%          1. meanDeltaPowerVsTime 
+%          2. deltaPowerVsSlope
+% In immediete future aim is to develop it further to include the slope
+
+% total no of trials = 60 (5*12)
+% Duration of trial  = 50 sec
+
+% trialtypes: Valid: 1, Invalid: 2, Constant: 3;
+
+fontsize = 14;
+
+hfig4 = figure(5);
+hAverageDeltaPowerVsTime  = subplot(1,2,1);
+hdeltaSlopeVsSlopeInvalid = subplot(1,2,2);
+
+startTrialTimePos = 13; % default one
+
+averageDeltaPowerVsTime(startTrialTimePos,hAverageDeltaPowerVsTime,fontsize);
+% slopeAnalysis_deltaSlopeVsSlopeInvalid(hdeltaSlopeVsSlopeInvalid,fontsize);
+slopeAnalysis_deltaSlopeVsSlopeInvalid_v2(hdeltaSlopeVsSlopeInvalid,fontsize);
+
+%--------------------------------------------------------------------------
+% Set axis properties
+hplot = hdeltaSlopeVsSlopeInvalid;
+subplot(hplot);
+set(hplot,'box','off'...
+    ,'fontsize',fontsize...
+    ,'TickDir','out'...
+    ,'TickLength',[0.03 0.03]...
+    ,'linewidth',1.2...
+    ,'xcolor',[0 0 0]...
+    ,'ycolor',[0 0 0]...
+    );
+
+hplot = hAverageDeltaPowerVsTime;
+subplot(hplot);
+set(hplot,'box','off'...
+    ,'fontsize',fontsize...
+    ,'TickDir','out'...
+    ,'TickLength',[0.03 0.03]...
+    ,'linewidth',1.2...
+    ,'xcolor',[0 0 0]...
+    ,'ycolor',[0 0 0]...
+    );
+
+
+
